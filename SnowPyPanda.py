c6d0e28f1ca9d9d883eb74dd97ba606cd5b6d1bf
koronicsnorbert <koronics.norbert@gmail.com>
2022-06-12 13:49:33 +0200

    GIT Push konfigok beállítva

--- SnowPyPanda.py
+++ SnowPyPanda.py
@@ -1,15 +1,32 @@
 import requests
 import json
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+import os
 
-### - Incidens Table API SNurl = "https://auchanhu.service-now.com/api/now/table/incident?sysparm_query=resolved_atONLast%20month%40javascript%3Ags.beginningOfLastMonth()%40javascript%3Ags.endOfLastMonth()%5Eassigned_to.manager%3Db7302538dba9b410d3640342f3961997%5Eassignment_group%3D6c37d064db2acb0072522de74b961918&sysparm_limit=1"
-SNurl = "https://auchanhu.service-now.com/api/now/table/incident?sysparm_query=resolved_atONLast%20month%40javascript%3Ags.beginningOfLastMonth()%40javascript%3Ags.endOfLastMonth()%5Eassigned_to.manager%3Db7302538dba9b410d3640342f3961997%5Eassignment_group%3D6c37d064db2acb0072522de74b961918&sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_limit=10"
-SNuser = "norbert.koronics"
-SNpass = "Auchan88!!"
 
-r=requests.get(SNurl, auth=(SNuser, SNpass))
-rawjson = r.json()
-preaty_json = json.dumps(r.json(), indent=2)
-#print(preaty_json)
-#for tickets in rawjson["result"]:
-#    print(tickets["resolved_by"][0]["number"])
-print(rawjson["result"][0]["number"]["resolved_by"])
+# Setups
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('mode.chained_assignment', None)
+load_dotenv('.env')  # take environment variables from .env.
+
+r=requests.get(os.getenv("SNurl"), auth=(os.getenv("SNuser"), os.getenv("SNpass")))
+rawjson = json.loads(r.text)
+
+df = pd.json_normalize(rawjson, record_path =['result']).replace(r'^s*$', float('NaN'), regex = True)
+#print(df)
+pivot = df[['resolved_by.display_value', 'number', 'business_duration','subcategory','u_sulyozas_mero']]
+#print(pivot)
+
+pivot["u_sulyozas_mero"] = pivot["u_sulyozas_mero"].astype(float)
+pivot["business_duration"] = pd.to_timedelta(pivot['business_duration'])
+
+
+print(pd.pivot_table(pivot,
+                     index = ["resolved_by.display_value"],
+                     values = ["u_sulyozas_mero","business_duration"],
+                     aggfunc =dict(u_sulyozas_mero=np.sum, business_duration=np.mean)))
+
